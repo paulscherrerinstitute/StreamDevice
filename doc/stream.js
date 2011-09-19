e3dd0a319ae536b5f94ff98f0900998e93c10640
zimoch <zimoch>
2011-09-19 09:30:00 +0000

    updates

--- doc/stream.js
+++
@@ -1,5 +0,0 @@
-if (parent.head) {
-    parent.head.document.getElementById('subtitle').innerHTML=document.title;
-    parent.document.title=document.title;
-    document.getElementsByTagName('h1')[0].style.display="none";
-}
