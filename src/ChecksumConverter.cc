5ce9b737cfb214bc3dd98ab8d64c6f1744c28e83
zimoch <zimoch>
2011-10-13 15:42:00 +0000

    remove compiler warning

--- src/ChecksumConverter.cc
+++ src/ChecksumConverter.cc
@@ -545,9 +545,9 @@ printPseudo(const StreamFormat& format, StreamBuffer& output)
     debug("ChecksumConverter %s: output to check: \"%s\"\n",
         checksumMap[fnum].name, output.expand(start,length)());
 
-    sum = checksumMap[fnum].xorout ^ checksumMap[fnum].func(
+    sum = (checksumMap[fnum].xorout ^ checksumMap[fnum].func(
         reinterpret_cast<uchar*>(output(start)), length,
-        checksumMap[fnum].init) & mask[checksumMap[fnum].bytes];
+        checksumMap[fnum].init)) & mask[checksumMap[fnum].bytes];
 
     debug("ChecksumConverter %s: output checksum is 0x%lX\n",
         checksumMap[fnum].name, sum);
@@ -606,9 +606,9 @@ scanPseudo(const StreamFormat& format, StreamBuffer& input, long& cursor)
         return -1;
     }
 
-    sum = checksumMap[fnum].xorout ^ checksumMap[fnum].func(
+    sum = (checksumMap[fnum].xorout ^ checksumMap[fnum].func(
         reinterpret_cast<uchar*>(input(start)), length,
-        checksumMap[fnum].init) & mask[checksumMap[fnum].bytes];
+        checksumMap[fnum].init)) & mask[checksumMap[fnum].bytes];
 
     debug("ChecksumConverter %s: input checksum is 0x%0*lX\n",
         checksumMap[fnum].name, 2*checksumMap[fnum].bytes, sum);
