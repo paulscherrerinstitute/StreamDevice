a34678cbcc423d841f40e5c5e6ae46226b18bf52
zimoch <zimoch>
2012-05-15 13:36:14 +0000

    for 3.13 don't create variable and registrar entries

--- src/makedbd.pl
+++ src/makedbd.pl
@@ -1,6 +1,10 @@
+if (@ARGV[0] == "-3.13") {
+    shift;
+} else {
+    print "variable(streamDebug, int)\n";
+    print "registrar(streamRegistrar)\n";
+}
+print "driver(stream)\n";
 for (@ARGV) {
      print "device($_,INST_IO,dev${_}Stream,\"stream\")\n";
 }
-print "driver(stream)\n";
-print "variable(streamDebug, int)\n";
-print "registrar(streamRegistrar)\n";
