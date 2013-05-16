b907e3f469114c5833c3c5b081424c3e5826b620
zimoch <zimoch>
2013-05-16 08:26:01 +0000

    changed shortcut for PRINTF_SIZE_T_PREFIX from S to P. Some OS use S already.

--- src/StreamCore.cc
+++ src/StreamCore.cc
@@ -23,7 +23,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-#define S PRINTF_SIZE_T_PREFIX
+#define P PRINTF_SIZE_T_PREFIX
 
 enum Commands { end_cmd, in_cmd, out_cmd, wait_cmd, event_cmd, exec_cmd,
     connect_cmd, disconnect_cmd };
@@ -985,7 +985,7 @@ readCallback(StreamIoStatus status,
             finishProtocol(ReplyTimeout);
             return 0;
         case StreamIoFault:
-            error("%s: I/O error after reading %"S"d byte%s: \"%s%s\"\n",
+            error("%s: I/O error after reading %"P"d byte%s: \"%s%s\"\n",
                 name(),
                 inputBuffer.length(), inputBuffer.length()==1 ? "" : "s",
                 inputBuffer.length() > 20 ? "..." : "",
@@ -994,7 +994,7 @@ readCallback(StreamIoStatus status,
             return 0;
     }
     inputBuffer.append(input, size);
-    debug("StreamCore::readCallback(%s) inputBuffer=\"%s\", size %"S"d\n",
+    debug("StreamCore::readCallback(%s) inputBuffer=\"%s\", size %"P"d\n",
         name(), inputBuffer.expand()(), inputBuffer.length());
     if (*activeCommand != in_cmd)
     {
