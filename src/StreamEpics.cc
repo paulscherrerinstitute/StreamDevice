paulscherrerinstitute/StreamDevice

commit 9f6d4ab63f886e50c1fd1f215d29868d2d320d84

changed streamScanfN to return number of read bytes

src/StreamEpics.cc
@@ -157,7 +157,7 @@ class Stream : protected StreamCore
     long initRecord(const char* filename, const char* protocol,
         const char* busname, int addr, const char* busparam);
     bool print(format_t *format, va_list ap);
-    bool scan(format_t *format, void* pvalue, size_t maxStringSize);
+    long scan(format_t *format, void* pvalue, size_t maxStringSize);
     bool process();
 
 // device support functions
@@ -554,19 +554,21 @@ long streamPrintf(dbCommon *record, format_t *format, ...)
 long streamScanfN(dbCommon* record, format_t *format,
     void* value, size_t maxStringSize)
 {
+    long size;
     debug("streamScanfN(%s,format=%%%c,maxStringSize=%ld)\n",
         record->name, format->priv->conv, (long)maxStringSize);
     Stream* pstream = (Stream*)record->dpvt;
     if (!pstream) return ERROR;
-    if (!pstream->scan(format, value, maxStringSize))
+    size = pstream->scan(format, value, maxStringSize);
+    if (size == ERROR)
     {
-        return ERROR;
+        debug("streamScanfN(%s) failed\n", record->name);
     }
 #ifndef NO_TEMPORARY
     debug("streamScanfN(%s) success, value=\"%s\"\n",
         record->name, StreamBuffer((char*)value).expand()());
 #endif
-    return OK;
+    return size;
 }
 
 // Stream methods ////////////////////////////////////////////////////////
@@ -800,11 +802,12 @@ print(format_t *format, va_list ap)
     return false;
 }
 
-bool Stream::
+long Stream::
 scan(format_t *format, void* value, size_t maxStringSize)
 {
     // called by streamScanfN
 
+    unsigned long size = maxStringSize;
     // first remove old value from inputLine (if we are scanning arrays)
     consumedInput += currentValueLength;
     currentValueLength = 0;
@@ -820,20 +823,17 @@ scan(format_t *format, void* value, size_t maxStringSize)
             break;
         case DBF_STRING:
             currentValueLength  = scanValue(*format->priv, (char*)value,
-                maxStringSize);
+                size);
             break;
         default:
             error("INTERNAL ERROR (%s): Illegal format type\n", name());
-            return false;
-    }
-    if (currentValueLength < 0)
-    {
-        currentValueLength = 0;
-        return false;
+            return ERROR;
     }
     // Don't remove scanned value from inputLine yet, because
     // we might need the string in a later error message.
-    return true;
+    if (currentValueLength == ERROR) return ERROR;
+    if (format->type == DBF_STRING) return size;
+    return OK;
 }
 
 // epicsTimerNotify virtual method ///////////////////////////////////////
@@ -1163,6 +1163,7 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
     int status;
     const char* putfunc;
     format_s fmt;
+    unsigned long stringsize = MAX_STRING_SIZE;
 
     fmt.type = dbfMapping[format.type];
     fmt.priv = &format;
@@ -1172,10 +1173,18 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
         // to field of this or other record.
         StreamBuffer fieldBuffer;
         DBADDR* pdbaddr = (DBADDR*)fieldaddress;
-        long nord;
-        long nelem = pdbaddr->no_elements;
-        size_t size = nelem * dbValueSize(fmt.type);
-        buffer = fieldBuffer.clear().reserve(size);
+        size_t size;
+        unsigned long nord;
+        unsigned long nelem = pdbaddr->no_elements;
+        if (format.type == string_format &&
+            (pdbaddr->field_type == DBF_CHAR || pdbaddr->field_type == DBF_UCHAR))
+        {
+            // string to char array
+            size = nelem;
+        }
+        else
+            size = nelem * dbValueSize(fmt.type);
+        buffer = fieldBuffer.clear().reserve(size);  // maybe write to field directly in case types match?
         for (nord = 0; nord < nelem; nord++)
         {
             debug("Stream::matchValue(%s): buffer before: %s\n",
@@ -1232,24 +1241,27 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
                 }
                 case string_format:
                 {
-                    if (pdbaddr->field_type == DBF_CHAR)
+                    if (pdbaddr->field_type == DBF_CHAR ||
+                        pdbaddr->field_type == DBF_UCHAR)
                     {
                         // string to char array
-                        consumed = scanValue(format, buffer, nelem);
+                        stringsize = nelem;
+                        consumed = scanValue(format, buffer, stringsize);
                         debug("Stream::matchValue(%s): %s.%s = \"%.*s\"\n",
                                 name(), pdbaddr->precord->name,
                                 ((dbFldDes*)pdbaddr->pfldDes)->name,
                                 (int)consumed, buffer);
-                        nord = nelem;
+                        nord = nelem; // this shortcuts the loop
                     }
                     else
                     {
+                        stringsize = MAX_STRING_SIZE;
                         consumed = scanValue(format,
-                            buffer+MAX_STRING_SIZE*nord, MAX_STRING_SIZE);
+                            buffer+MAX_STRING_SIZE*nord, stringsize);
                         debug("Stream::matchValue(%s): %s.%s[%li] = \"%.*s\"\n",
                                 name(), pdbaddr->precord->name,
                                 ((dbFldDes*)pdbaddr->pfldDes)->name,
-                                nord, MAX_STRING_SIZE, buffer+MAX_STRING_SIZE*nord);
+                                nord, (int)stringsize, buffer+MAX_STRING_SIZE*nord);
                     }
                     break;
                 }
@@ -1290,7 +1302,7 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
             /* convert from Unix epoch (1 Jan 1970) to EPICS epoch (1 Jan 1990) */
             dval = dval-631152000u;
             pdbaddr->precord->time.secPastEpoch = (long)dval;
-            // rouding: we don't have 9 digits precision
+            // rounding: we don't have 9 digits precision
             // in a double of today's number of seconds
             pdbaddr->precord->time.nsec = (long)((dval-(long)dval)*1e6)*1000;
             debug("Stream::matchValue(%s): writing %i.%i to %s.TIME field\n",
@@ -1307,10 +1319,11 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
 #endif
         }
         if (format.type == string_format &&
-            (pdbaddr->field_type == DBF_CHAR || pdbaddr->field_type == DBF_UCHAR))
+            (pdbaddr->field_type == DBF_CHAR ||
+            pdbaddr->field_type == DBF_UCHAR))
         {
             /* write strings to [U]CHAR arrays */
-            nord = consumed;
+            nord = stringsize;
             fmt.type = DBF_CHAR;
         }
         if (pdbaddr->precord == record || INIT_RUN)
@@ -1349,25 +1362,26 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
                 case DBF_ULONG:
                 case DBF_LONG:
                 case DBF_ENUM:
-                    error("%s: %s(%s.%s, %s, %li) failed\n",
+                    error("%s: %s(%s.%s, %s, %li, %lu) failed\n",
                         name(), putfunc, pdbaddr->precord->name,
                         ((dbFldDes*)pdbaddr->pfldDes)->name,
                         pamapdbfType[fmt.type].strvalue,
-                        lval);
+                        lval, nord);
                     return false;
                 case DBF_DOUBLE:
-                    error("%s: %s(%s.%s, %s, %#g) failed\n",
+                    error("%s: %s(%s.%s, %s, %#g, %lu) failed\n",
                         name(), putfunc, pdbaddr->precord->name,
                         ((dbFldDes*)pdbaddr->pfldDes)->name,
                         pamapdbfType[fmt.type].strvalue,
-                        dval);
+                        dval, nord);
                     return false;
                 case DBF_STRING:
-                    error("%s: %s(%s.%s, %s, \"%.*s\") failed\n",
+                case DBF_CHAR:
+                    error("%s: %s(%s.%s, %s, \"%.*s\", %lu) failed\n",
                         name(), putfunc, pdbaddr->precord->name,
                         ((dbFldDes*)pdbaddr->pfldDes)->name,
                         pamapdbfType[fmt.type].strvalue,
-                        (int)consumed, buffer);
+                        (int)consumed, buffer, nord);
                     return false;
                 default:
                     return false;

src/BCDConverter.cc
@@ -27,7 +27,7 @@ class BCDConverter : public StreamFormatConverter
 {
     int parse (const StreamFormat&, StreamBuffer&, const char*&, bool);
     bool printLong(const StreamFormat&, StreamBuffer&, long);
-    int scanLong(const StreamFormat&, const char*, long&);
+    long scanLong(const StreamFormat&, const char*, long&);
 };
 
 int BCDConverter::
@@ -39,64 +39,65 @@ parse(const StreamFormat& fmt, StreamBuffer&, const char*&, bool)
 bool BCDConverter::
 printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
 {
-    unsigned char bcd[6]={0,0,0,0,0,0}; // sufficient for 2^32
-    int i;
-    int prec = fmt.prec; // number of nibbles
-    if (prec == -1)
-    {
-        prec = 2 * sizeof (value);
-    }
-    int width = (prec + (fmt.flags & sign_flag ? 2 : 1)) / 2;
+    unsigned char bcd;
+    bool neg = false;
+    long i;
+    unsigned long prec = fmt.prec < 0 ? 2 * sizeof(value) : fmt.prec; // number of nibbles
+    unsigned long width = (prec + (fmt.flags & sign_flag ? 2 : 1)) / 2;
     if (fmt.width > width) width = fmt.width;
-    if (fmt.flags & sign_flag && value < 0)
+    if (fmt.flags & sign_flag && value < 0 && prec > 0)
     {
-        // negative BCD value, I hope "F" as "-" is OK
-        bcd[5] = 0xF0;
+        neg = true;
         value = -value;
-    }
-    if (prec > 10) prec = 10;
-    for (i = 0; i < prec; i++)
-    {
-        bcd[i/2] |= (value % 10) << (4 * (i & 1));
-        value /= 10;
     }
     if (fmt.flags & alt_flag)
     {
         // least significant byte first (little endian)
-        for (i = 0; i < (prec + 1) / 2; i++)
+        while (width-- && prec)
         {
-            output.append(bcd[i]);
+            bcd = value%10;
+            if (--prec)
+            {
+                --prec;
+                value /= 10;
+                bcd |= (value%10)<<4;
+                value /= 10;
+            }
+            output.append(bcd);
         }
-        for (; i < width; i++)
-        {
-            output.append('\0');
-        }
-        output[-1] |=  bcd[5];
+        if (width)
+            output.append('\0', width);
+        if (neg) output[-1] |= 0xf0;
     }
     else
     {
         // most significant byte first (big endian)
-        int firstbyte = output.length();
-        for (i = 0; i < width - (prec + 1) / 2; i++)
+        output.append('\0', width);
+        if (neg) output[-width] |= 0xf0;
+        i = 0;
+        while (width-- && prec)
         {
-            output.append('\0');
+            bcd = value%10;
+            if (--prec)
+            {
+                --prec;
+                value /= 10;
+                bcd |= (value%10)<<4;
+                value /= 10;
+            }
+            output[--i]=bcd;
         }
-        for (i = (prec - 1) / 2; i >= 0; i--)
-        {
-            output.append(bcd[i]);
-        }
-        output[firstbyte] |= bcd[5];
     }
     return true;
 }
 
-int BCDConverter::
+long BCDConverter::
 scanLong(const StreamFormat& fmt, const char* input, long& value)
 {
-    int length = 0;
-    int val = 0;
+    long length = 0;
+    long val = 0;
     unsigned char bcd1, bcd10;
-    int width = fmt.width;
+    long width = fmt.width;
     if (width == 0) width = 1;
     if (fmt.flags & alt_flag)
     {

src/BinaryConverter.cc
@@ -29,7 +29,7 @@ class BinaryConverter : public StreamFormatConverter
 {
     int parse(const StreamFormat&, StreamBuffer&, const char*&, bool);
     bool printLong(const StreamFormat&, StreamBuffer&, long);
-    int scanLong(const StreamFormat&, const char*, long&);
+    long scanLong(const StreamFormat&, const char*, long&);
 };
 
 int BinaryConverter::
@@ -77,7 +77,7 @@ printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
         if (x <= 0x3FFFFFFF) { prec -= 2; x <<=2; }
         if (x <= 0x7FFFFFFF) { prec -= 1; }
     }
-    int width = prec;
+    unsigned long width = prec;
     if (fmt.width > width) width = fmt.width;
     char zero = fmt.info[0];
     char one = fmt.info[1];
@@ -88,7 +88,7 @@ printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
         if (!(fmt.flags & left_flag))
         {
             // pad left
-            while (width > prec)
+            while (width > (unsigned int)prec)
             {
                 output.append(' ');
                 width--;
@@ -112,7 +112,7 @@ printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
         if (!(fmt.flags & left_flag))
         {
             // pad left
-            while (width > prec)
+            while (width > (unsigned int)prec)
             {
                 output.append(fill);
                 width--;
@@ -132,13 +132,13 @@ printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
     return true;
 }
 
-int BinaryConverter::
+long BinaryConverter::
 scanLong(const StreamFormat& fmt, const char* input, long& value)
 {
     long val = 0;
-    int width = fmt.width;
+    long width = fmt.width;
     if (width == 0) width = -1;
-    int length = 0;
+    long length = 0;
     char zero = fmt.info[0];
     char one = fmt.info[1];
     if (!isspace(zero) && !isspace(one))

src/ChecksumConverter.cc
@@ -500,7 +500,7 @@ class ChecksumConverter : public StreamFormatConverter
 {
     int parse (const StreamFormat&, StreamBuffer&, const char*&, bool);
     bool printPseudo(const StreamFormat&, StreamBuffer&);
-    int scanPseudo(const StreamFormat&, StreamBuffer&, long& cursor);
+    long scanPseudo(const StreamFormat&, StreamBuffer&, long& cursor);
 };
 
 int ChecksumConverter::
@@ -535,7 +535,7 @@ parse(const StreamFormat&, StreamBuffer& info, const char*& source, bool)
         source+=3;
         notflag = true;
     }
-    unsigned  fnum;
+    unsigned char fnum;
     int len = p-source;
     unsigned int init, xorout;
     for (fnum = 0; fnum < sizeof(checksumMap)/sizeof(checksum); fnum++)
@@ -573,7 +573,7 @@ printPseudo(const StreamFormat& format, StreamBuffer& output)
     const char* info = format.info;
     unsigned int init = extract<unsigned int>(info);
     unsigned int xorout = extract<unsigned int>(info);
-    int fnum = extract<char>(info);
+    unsigned char fnum = extract<unsigned char>(info);
 
     int start = format.width;
     int length = output.length()-format.width;
@@ -641,7 +641,7 @@ printPseudo(const StreamFormat& format, StreamBuffer& output)
     return true;
 }
 
-int ChecksumConverter::
+long ChecksumConverter::
 scanPseudo(const StreamFormat& format, StreamBuffer& input, long& cursor)
 {
     unsigned int sum;
@@ -649,8 +649,8 @@ scanPseudo(const StreamFormat& format, StreamBuffer& input, long& cursor)
     unsigned int init = extract<unsigned int>(info);
     unsigned int xorout = extract<unsigned int>(info);
     int start = format.width;
-    int fnum = extract<char>(info);
-    int length = cursor-format.width;
+    unsigned char fnum = extract<unsigned char>(info);
+    long length = cursor-format.width;
 
     if (format.prec > 0) length -= format.prec;
     

src/EnumConverter.cc
@@ -29,7 +29,7 @@ class EnumConverter : public StreamFormatConverter
 {
     int parse(const StreamFormat&, StreamBuffer&, const char*&, bool);
     bool printLong(const StreamFormat&, StreamBuffer&, long);
-    int scanLong(const StreamFormat&, const char*, long&);
+    long scanLong(const StreamFormat&, const char*, long&);
 };
 
 // info format: <numEnums><index><string>0<index><string>0...
@@ -148,7 +148,7 @@ printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
     return true;
 }
 
-int EnumConverter::
+long EnumConverter::
 scanLong(const StreamFormat& fmt, const char* input, long& value)
 {
     debug("EnumConverter::scanLong(%%%c, \"%s\")\n",
@@ -156,7 +156,7 @@ scanLong(const StreamFormat& fmt, const char* input, long& value)
     const char* s = fmt.info;
     long numEnums = extract<long>(s);
     long index;
-    int length;
+    long length;
 
     bool match;
     while (numEnums--)

src/MantissaExponentConverter.cc
@@ -39,7 +39,7 @@
 class MantissaExponentConverter : public StreamFormatConverter
 {
     virtual int parse(const StreamFormat&, StreamBuffer&, const char*&, bool);
-    virtual int scanDouble(const StreamFormat&, const char*, double&);
+    virtual long scanDouble(const StreamFormat&, const char*, double&);
     virtual bool printDouble(const StreamFormat&, StreamBuffer&, double);
 };
 
@@ -50,7 +50,7 @@ parse(const StreamFormat&, StreamBuffer&,
     return double_format;
 }
 
-int MantissaExponentConverter::
+long MantissaExponentConverter::
 scanDouble(const StreamFormat& fmt, const char* input, double& value)
 {
     int mantissa;

src/RawConverter.cc
@@ -27,7 +27,7 @@ class RawConverter : public StreamFormatConverter
 {
     int parse(const StreamFormat&, StreamBuffer&, const char*&, bool);
     bool printLong(const StreamFormat&, StreamBuffer&, long);
-    int scanLong(const StreamFormat&, const char*, long&);
+    long scanLong(const StreamFormat&, const char*, long&);
 };
 
 int RawConverter::
@@ -40,10 +40,9 @@ parse(const StreamFormat& fmt, StreamBuffer&,
 bool RawConverter::
 printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
 {
-    int prec = fmt.prec;      // number of bytes from value
-    if (prec == -1) prec = 1;    // default: 1 byte
-    int width = prec;            // number of bytes in output
-    if (prec > (int)sizeof(long)) prec=sizeof(long);
+    unsigned int prec = fmt.prec < 0 ? 1 : fmt.prec; // number of bytes from value, default 1
+    unsigned long width = prec;  // number of bytes in output
+    if (prec > sizeof(long)) prec=sizeof(long);
     if (fmt.width > width) width = fmt.width;
     
     char byte = 0;
@@ -96,12 +95,12 @@ printLong(const StreamFormat& fmt, StreamBuffer& output, long value)
     return true;
 }
 
-int RawConverter::
+long RawConverter::
 scanLong(const StreamFormat& fmt, const char* input, long& value)
 {
     long length = 0;
     long val = 0;
-    int width = fmt.width;
+    unsigned long width = fmt.width;
     if (width == 0) width = 1; // default: 1 byte
     if (fmt.flags & skip_flag)
     {

src/RawFloatConverter.cc
@@ -29,7 +29,7 @@ class RawFloatConverter : public StreamFormatConverter
 {
     int parse(const StreamFormat&, StreamBuffer&, const char*&, bool);
     bool printDouble(const StreamFormat&, StreamBuffer&, double);
-    int scanDouble(const StreamFormat&, const char*, double&);
+    long scanDouble(const StreamFormat&, const char*, double&);
 };
 
 int RawFloatConverter::
@@ -90,7 +90,7 @@ printDouble(const StreamFormat& format, StreamBuffer& output, double value)
     return true;
 }
 
-int RawFloatConverter::
+long RawFloatConverter::
 scanDouble(const StreamFormat& format, const char* input, double& value)
 {
     int nbOfBytes;

commit 1120651daa80cabe7d95e9d37c35db79ffbc0065

fixed bug with arrays introduced in commit 9f6d4ab63f886e50c1fd1f215d29868d2d320d84

src/StreamEpics.cc
@@ -586,19 +586,9 @@ ssize_t streamScanfN(dbCommon* record, format_t *format,
     void* value, size_t maxStringSize)
 {
     ssize_t size;
-    debug("streamScanfN(%s,format=%%%c,maxStringSize=%ld)\n",
-        record->name, format->priv->conv, (long)maxStringSize);
     Stream* pstream = (Stream*)record->dpvt;
     if (!pstream) return ERROR;
     size = pstream->scan(format, value, maxStringSize);
-    if (size == ERROR)
-    {
-        debug("streamScanfN(%s) failed\n", record->name);
-    }
-#ifndef NO_TEMPORARY
-    debug("streamScanfN(%s) success, value=\"%s\"\n",
-        record->name, StreamBuffer((char*)value).expand()());
-#endif
     return size;
 }
 
@@ -828,9 +818,11 @@ print(format_t *format, va_list ap)
             return printValue(*format->priv, va_arg(ap, double));
         case DBF_STRING:
             return printValue(*format->priv, va_arg(ap, char*));
+        default:
+            error("INTERNAL ERROR (%s): Illegal format type %d\n",
+                name(), format->type);
+            return false;
     }
-    error("INTERNAL ERROR (%s): Illegal format type\n", name());
-    return false;
 }
 
 ssize_t Stream::
@@ -839,7 +831,7 @@ scan(format_t *format, void* value, size_t maxStringSize)
     // called by streamScanfN
 
     size_t size = maxStringSize;
-    // first remove old value from inputLine (if we are scanning arrays)
+    // first remove old value from inputLine (in case we are scanning arrays)
     consumedInput += currentValueLength;
     currentValueLength = 0;
     switch (format->type)
@@ -853,16 +845,21 @@ scan(format_t *format, void* value, size_t maxStringSize)
             currentValueLength = scanValue(*format->priv, *(double*)value);
             break;
         case DBF_STRING:
-            currentValueLength  = scanValue(*format->priv, (char*)value,
-                size);
+            currentValueLength  = scanValue(*format->priv, (char*)value, size);
             break;
         default:
-            error("INTERNAL ERROR (%s): Illegal format type\n", name());
+            error("INTERNAL ERROR (%s): Illegal format type %d\n",
+                name(), format->type);
             return ERROR;
+    }
+    debug("Stream::scan() currentValueLength=%" Z "d\n", currentValueLength);
+    if (currentValueLength < 0) 
+    {
+        currentValueLength = 0; // important for arrays with less than NELM elements
+        return ERROR;
     }
     // Don't remove scanned value from inputLine yet, because
     // we might need the string in a later error message.
-    if (currentValueLength == ERROR) return ERROR;
     if (format->type == DBF_STRING) return size;
     return OK;
 }
@@ -1442,6 +1439,7 @@ matchValue(const StreamFormat& format, const void* fieldaddress)
     }
     flags |= ValueReceived;
     consumedInput += currentValueLength;
+    debug("Stream::matchValue(%s): success, %" Z "d bytes consumed\n", name(), currentValueLength);
     return true;
 }
 