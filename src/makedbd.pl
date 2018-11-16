8f34dd2c847e7fafa4250634f0d5e880efadf1bf
Dirk Zimoch <dirk.zimoch@psi.ch>
2018-11-16 10:28:46 +0100

    avoid problems with echo and quotes in Windows

--- src/makedbd.pl
+++ src/makedbd.pl
@@ -1,9 +1,14 @@
-if (@ARGV[0] == "-3.13") {
+if (@ARGV[0] eq "--with-asyn") {
+    shift;
+    $asyn = 1;
+}
+if (@ARGV[0] eq "-3.13") {
     shift;
 } else {
     print "variable(streamDebug, int)\n";
     print "variable(streamError, int)\n";
     print "registrar(streamRegistrar)\n";
+    if ($asyn) { print "registrar(AsynDriverInterfaceRegistrar)\n"; }
 }
 print "driver(stream)\n";
 for (@ARGV) {
